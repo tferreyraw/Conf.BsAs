945a5d1a18ba7a6a636c72286a8dda95ae900c96
tferreyraw <68830048+tferreyraw@users.noreply.github.com>
2022-10-29 20:01:08 -0300

    Agrego Javascript
    genero cambios en los id de html y le coloco la funcionalidad con JS

--- pages/tickets.js
+++ pages/tickets.js
@@ -1,9 +1,33 @@
-const valorTicket = 200;
-const total = 1
-const descuentoEstudiante = 0.8;
-const descuentoTrainee = 0.5;
-const descuentoJunior = 0.15;
+const cantidadInput = document.querySelector("#cantidad");
+const categoriaInput = document.querySelector("#categoria");
+const resumenButton = document.querySelector("#resumen");
 
-function calcularPrecio(categoria, cantidad){
-  
-}
+const valorTicket = 200;
+const descuentos = {
+  estudiante: 0.8,
+  trainee: 0.5,
+  junior: 0.15,
+};
+
+const valoresForm = {
+  cantidad: 0,
+  categoria: "estudiante",
+};
+
+const calcularPrecio = (cantidad, categoria) =>
+  (valorTicket - valorTicket * descuentos[categoria]) * cantidad;
+
+cantidadInput.addEventListener("change", (ev) => {
+  valoresForm.cantidad = Number(ev.target.value);
+});
+
+categoriaInput.addEventListener("change", (ev) => {
+  valoresForm.categoria = ev.target.value;
+});
+
+resumenButton.addEventListener("click", () => {
+  document.querySelector("#total").innerText = calcularPrecio(
+    valoresForm.cantidad,
+    valoresForm.categoria
+  );
+});
