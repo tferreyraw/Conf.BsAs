61cd44b317272b04befe74fdb2b98e96970189b0
tferreyraw <68830048+tferreyraw@users.noreply.github.com>
2022-10-29 18:58:38 -0300

    Arreglos
    CAmbios en la pagina principal y agrego pagina de tickets

---
+++ pages/tickets.js
@@ -0,0 +1,9 @@
+const valorTicket = 200;
+const total = 1
+const descuentoEstudiante = 0.8;
+const descuentoTrainee = 0.5;
+const descuentoJunior = 0.15;
+
+function calcularPrecio(categoria, cantidad){
+  
+}
